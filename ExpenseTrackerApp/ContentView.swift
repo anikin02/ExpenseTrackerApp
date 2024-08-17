01295238fddecae88cdba414a64b5c8adab95328
Danil Anikin <mart4r.clearlist@gmail.com>
2024-08-17 17:21:14 +1000

    Initial Commit

---
+++ ExpenseTrackerApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExpenseTrackerApp
+//
+//  Created by Данил Аникин on 17.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExpenseTrackerApp/ExpenseTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerAppApp.swift
+//  ExpenseTrackerApp
+//
+//  Created by Данил Аникин on 17.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
